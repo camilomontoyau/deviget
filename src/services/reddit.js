455c81a4a8741bbeb3955a4f8981b54db78c779f
camilo <camilomontoya2@gmail.com>
2019-09-20 19:02:33 -0500

    reddit as a reusable module

---
+++ src/services/reddit.js
@@ -0,0 +1,9 @@
+import snoowrap from "snoowrap";
+const r = new snoowrap({
+  userAgent: "deviget-test2",
+  clientId: "TIPuc7DsPEgEwA",
+  clientSecret: "QtIYW3DpMwbXCjfJeFONI_8fyxk",
+  refreshToken: "81058191780-23buKHuO4lGTricyV5L6xI1yOAY"
+});
+
+export default r;
